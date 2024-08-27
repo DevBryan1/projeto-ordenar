1c6f1ed3cc3c9c2890271ee1fe66a6b0274579cd
DevBryan1 <bryanmellodoc@gmail.com>
2024-08-27 13:32:10 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+$dns = "mysql:dbname=projeto_ordenar;host=localhost;";
+$dbuser = "root";
+$dbpass = "";
+
+try{
+    $pdo = new PDO($dns, $dbuser, $dbpass);
+
+} catch(PDOException $e) {
+    echo "ERRO: ".$e->getMassage();
+}
+
+if(isset($_GET['ordem']) && !empty($_GET['ordem'])){
+        $ordem = addslashes($_GET['ordem']);
+        $sql = "SELECT * FROM usuarios ORDER BY ".$ordem;
+    }else {
+        $ordem = ''; 
+        $sql = "SELECT * FROM usuarios";
+    }
+
+?>
+    <form method="GET">
+        <select name="ordem" onchange="this.form.submit()">
+            <option></option>
+            <option value="nome" <?php echo ($ordem == "nome" )?'selected="selected"':'' ?> >Pelo nome</option>
+            <option value="idade" <?php echo ($ordem == "idade" )?'selected="selected"':'' ?>>Pela idade</option>
+        </select>
+    </form>
+
+    <table border="1" width="400px">
+        <tr>
+            <th>Nome</th>
+            <th>Idade</th>
+        </tr>
+    <?php
+
+    $sql = $pdo->query($sql);
+    if($sql->rowCount() > 0){
+
+        foreach($sql->fetchAll() as $usuarios){
+            ?>
+            <tr>
+                <td><?php echo $usuarios['nome']?></td>
+                <td><?php echo $usuarios['idade']?></td>
+            </tr>
+    <?php
+}
+}
+?>
+</table>
